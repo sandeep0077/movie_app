cf8252defd6d7440b266343d3153a3b8e7a22c85
sandeep0077 <76220371+sandeep0077@users.noreply.github.com>
2021-03-21 21:04:21 +0530

    installed redux and imported redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import movies from './reducers/index'
+
+
+// we will pass the store in the argument
+const store = createStore(movies)
+console.log('store', store)
+console.log('State', store.getState())
+
 
 ReactDOM.render(
   <React.StrictMode>
